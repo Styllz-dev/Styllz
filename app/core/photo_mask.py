3f20e1125f04d80889328eb7bfb10aff148b5425
MrNereof <mrnereof@gmail.com>
2023-04-14 11:38:25 +0300

    Move photo_mask to new core folder

--- app/core/photo_mask.py
+++ app/core/photo_mask.py
@@ -1,14 +1,12 @@
+from io import BytesIO
+
 import cv2
 import mediapipe as mp
 import numpy as np
-import io
 from PIL import Image
 
 
-
-
-def MakePhotoMask(stream):
-
+def make_photo_mask(stream: BytesIO) -> BytesIO:
     raw_image = Image.open(stream)
 
     frame = np.asarray(raw_image)
@@ -18,7 +16,6 @@ def MakePhotoMask(stream):
     # for x in range(frame.shape[0]):
     #     for y in range(200):
     #         result[x][y] = [frame[x][y][0], frame[x][y][1], frame[x][y][2], 0]
-
 
     # cv2.imwrite('outputT.png', result)
 
@@ -51,42 +48,48 @@ def MakePhotoMask(stream):
     # Pose Detections
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
     landmark = results.pose_landmarks.landmark
+
     def distance(f, s, shape=frame.shape):
         x1, y1 = landmark[f].x * shape[1], landmark[f].y * shape[0]
         x2, y2 = landmark[s].x * shape[1], landmark[s].y * shape[0]
         return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
-
     presence = 0.25
     left_border = landmark[12].x * (1 - presence)
     right_border = landmark[11].x * (1 + presence)
 
-    middle = (left_border + right_border)/2
+    middle = (left_border + right_border) / 2
 
     def InBorder(landmarkIndex):
         return (left_border < landmark[landmarkIndex].x < right_border) and landmark[landmarkIndex].y < landmark[0].y
 
     pose_validated = False
 
-    if (landmark[12].y > landmark[24].y and landmark[24].y > landmark[26].y and landmark[26].y > landmark[28].y and # left side vertical
-        landmark[11].y > landmark[23].y and landmark[23].y > landmark[25].y and landmark[25].y > landmark[27].y and # right side vertical
-        landmark[12].x < middle and landmark[24].x < middle and landmark[26].x < middle and landmark[28].x < middle and # left side horizontal
-        landmark[11].x > middle and landmark[23].x > middle and landmark[25].x > middle and landmark[27].x > middle and # right side horizontal
-        landmark[8].x > landmark[12].x and landmark[8].y > landmark[12].y and landmark[8].x < landmark[7].x and landmark[7].x < landmark[11].x and landmark[7].y > landmark[11].y and # head
-        InBorder(14) and InBorder(16) and InBorder(18) and InBorder(20) and InBorder(22) and # left hand
-        InBorder(13) and InBorder(15) and InBorder(17) and InBorder(19) and InBorder(21) and # right hand
-        InBorder(24) and InBorder(26) and InBorder(28) and InBorder(30) and InBorder(32) and # left leg
-        InBorder(23) and InBorder(25) and InBorder(27) and InBorder(29) and InBorder(29) # right leg
-        ):
+    if (landmark[12].y > landmark[24].y > landmark[26].y > landmark[
+        28].y and  # left side vertical
+            landmark[11].y > landmark[23].y > landmark[25].y > landmark[
+                27].y and  # right side vertical
+            landmark[12].x < middle and landmark[24].x < middle and landmark[26].x < middle and landmark[
+                28].x < middle and  # left side horizontal
+            landmark[11].x > middle and landmark[23].x > middle and landmark[25].x > middle and landmark[
+                27].x > middle and  # right side horizontal
+            landmark[8].x > landmark[12].x and landmark[8].y > landmark[12].y and landmark[8].x < landmark[7].x <
+            landmark[11].x and landmark[7].y > landmark[11].y and  # head
+            InBorder(14) and InBorder(16) and InBorder(18) and InBorder(20) and InBorder(22) and  # left hand
+            InBorder(13) and InBorder(15) and InBorder(17) and InBorder(19) and InBorder(21) and  # right hand
+            InBorder(24) and InBorder(26) and InBorder(28) and InBorder(30) and InBorder(32) and  # left leg
+            InBorder(23) and InBorder(25) and InBorder(27) and InBorder(29) and InBorder(29)  # right leg
+    ):
         pose_validated = True
 
     pose_validated = True
     cutting_coef = 0.05
     if (pose_validated):
         top_border = max(landmark[12].y - cutting_coef, 0)
-        bottom_border = min(max([landmark[27].y, landmark[28].y, landmark[29].y, landmark[30].y, landmark[31].y, landmark[32].y]) + cutting_coef, 1)
+        bottom_border = min(max([landmark[27].y, landmark[28].y, landmark[29].y, landmark[30].y, landmark[31].y,
+                                 landmark[32].y]) + cutting_coef, 1)
 
-        print(top_border, bottom_border)
+        # print(top_border, bottom_border)
         # cutting person
 
         # atODO validate photo
@@ -100,13 +103,16 @@ def MakePhotoMask(stream):
             for y in range(int(top_border * frame.shape[0]), int(bottom_border * frame.shape[0])):
                 result[y][x] = [frame[y][x][0], frame[y][x][1], frame[y][x][2], 0]
 
-
         # cv2.imwrite('output.png', result)
 
     else:
-        print("Wrong pose")
+        pass
+        # print("Wrong pose")
 
-    final_stream = Image.fromarray(result)
+    final_image = Image.fromarray(result)
+
+    final_stream = BytesIO()
+    final_image.save(final_stream, format="PNG")
 
     return final_stream
 
@@ -114,8 +120,6 @@ def MakePhotoMask(stream):
 
     # cv2.waitKey(0)
     # cv2.destroyWindow("python")
-
-
 
     # poseDetector = mp.solutions.hands.Hands(
     #     static_image_mode=True,
