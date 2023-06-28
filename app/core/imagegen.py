2acfb870f21c1d36dd4eca0d851ed894bc34ebce
MrNereof <mrnereof@gmail.com>
2023-06-28 12:09:17 +0300

    remove duplicates

--- app/core/imagegen.py
+++ app/core/imagegen.py
@@ -12,7 +12,7 @@ def make_photo(prompt: Prompt):
         api = webuiapi.WebUIApi(**settings.SD_API)
         result = api.txt2img(
             prompt=f"a full-length portrait photo with face of (styllzuser{settings.SD_USER} man:1.2) wearing {prompt.style} {prompt.details} <lora:styllzuser{settings.SD_USER}:1>",
-            negative_prompt="(blue eyes, semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime:1.4), duplicate, fat, text, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck",
+            negative_prompt="(blue eyes, semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime:1.4), fat, text, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck",
             seed=3362803203,
             cfg_scale=5,
             sampler_index='DPM++ SDE Karras',
